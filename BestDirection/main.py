19b40d6f4a72db88cdb293527257a4e6fb626139
Datoclement <70664914@qq.com>
2017-10-07 19:55:38 +0200

    BestDirection

---
+++ BestDirection/main.py
@@ -0,0 +1,65 @@
+
+import numpy as np
+import vispy as vp
+import parser,sys
+import vispy.plot as plt
+import vispy.scene as scn
+vp.app.use_app('pyqt5')
+
+canvas = scn.SceneCanvas(keys='interactive',size=(600,600),show=True,bgcolor='white')
+
+view = canvas.central_widget.add_view()
+view.camera = scn.TurntableCamera()
+
+dtype = [\
+    ('model_id',int),\
+    ('azimuth',float),\
+    ('elevation',float),\
+    ('class',int)]
+def record_to_str(rc):
+    s = str(rc[0])
+    for i in range(1,len(rc)):
+        s += ' '+str(rc[i])
+    return s
+
+n = 1815
+cur_model_id = 0
+record_filename = 'best_worst_direction.txt'
+
+records = parser.load(record_filename)
+vs,fs,fc = parser.parse(cur_model_id)
+mesh = scn.visuals.Mesh(vertices=vs,faces=fs,vertex_colors=fc,parent=view.scene)
+
+@canvas.connect
+def on_key_press(event):
+    global view,mesh,cur_model_id,n,record_filename,records
+    print('-----')
+    ch = event.text
+    if(ch=='n' or ch=='p'):
+        cur_model_id = (cur_model_id + (1 if ch=='n' else n-1)) % n
+        vs,fs,fc = parser.parse(cur_model_id)
+        mesh.set_data(vertices=vs,faces=fs,vertex_colors=fc)
+        print(('next' if ch=='n' else 'previous')+' model -> model id: ' + str(cur_model_id))
+    if(ch=='b' or ch=='w'):
+        az = view.camera.azimuth
+        el = view.camera.elevation
+        cl = (int)(ch=='b')
+        records += [(cur_model_id,az,el,cl)]
+        print('record '+('best' if ch=='b' else 'worst') + ' direction')
+        print('model id, azumith, elevation: '+str(cur_model_id)+' '+str(az)+' '+str(el))
+    if(ch=='u'):
+        if (len(records) > 0):
+            print('undo: pop last record '+record_to_str(records[-1]))
+            print('existing records: '+str(len(records)))
+            del records[-1]
+    if(ch=='s'):
+        f = open(record_filename,'w')
+        s = ''
+        for rc in records:
+            s += record_to_str(rc) + '\n'
+        f.write(s)
+        f.close()
+        print('save records successfully')
+
+if __name__ == '__main__' and sys.flags.interactive != 1:
+    vp.app.run()
---
+++ BestDirection/parser.py
@@ -0,0 +1,41 @@
+import numpy as np
+import os
+def parse(index,database_path=''):
+    def line_parser(f):
+        s = f.readline()
+        return list(map(eval,s.strip().split(' ')))
+
+    relative = '../benchmark/db/'
+    partindex = str(index // 100)
+    fileindex = str(index % 100)
+    path = os.path.join(database_path,relative+partindex+'/m'+fileindex+'/m'+fileindex+'.off')
+
+    f = open(path,'r')
+    f.readline()
+    V,F,_ = line_parser(f)
+    vs = np.zeros(shape=(V,3))
+    fs = np.zeros(shape=(F,3),dtype=int)
+    for i in range(V):
+        vs[i] = np.array(line_parser(f))
+
+    for i in range(F):
+        fs[i] = np.array(line_parser(f))[1:4]
+
+    rng = np.random.RandomState(0)
+    fc = np.ones((V, 4), dtype=np.float32)
+    fc[:, 0] = np.linspace(1, 0, V)
+    fc[:, 1] = rng.randn(V)
+    fc[:, 2] = np.linspace(0, 1, V)
+
+    return vs,fs,fc
+
+def load(filename):
+    res = list()
+    if not os.path.isfile(filename):
+        return res
+
+    f = open(filename,'r')
+    for l in f:
+        res += [tuple(map(eval,l.strip().split(' ')))]
+
+    return res
