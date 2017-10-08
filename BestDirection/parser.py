41d1287f671764297378c2a4d00b7a9d0310b72b
iRiisH <vzs@hotmail.fr>
2017-10-08 19:17:59 +0200

    image generator

--- BestDirection/parser.py
+++ BestDirection/parser.py
@@ -1,20 +1,22 @@
 import numpy as np
 import os
-def parse(index,database_path=''):
+
+
+def parse(index, database_path=''):
     def line_parser(f):
         s = f.readline()
-        return list(map(eval,s.strip().split(' ')))
+        return list(map(eval, s.strip().split(' ')))
 
     relative = '../data/benchmark/db/'
     partindex = str(index // 100)
     fileindex = str(index)
-    path = os.path.join(database_path,relative+partindex+'/m'+fileindex+'/m'+fileindex+'.off')
+    path = os.path.join(database_path, relative+partindex+'/m'+fileindex+'/m'+fileindex+'.off')
 
-    f = open(path,'r')
+    f = open(path, 'r')
     f.readline()
-    V,F,_ = line_parser(f)
-    vs = np.zeros(shape=(V,3))
-    fs = np.zeros(shape=(F,3),dtype=int)
+    V, F, _ = line_parser(f)
+    vs = np.zeros(shape=(V, 3))
+    fs = np.zeros(shape=(F, 3), dtype=int)
     for i in range(V):
         vs[i] = np.array(line_parser(f))
 
@@ -27,15 +29,16 @@ def parse(index,database_path=''):
     fc[:, 1] = rng.randn(V)
     fc[:, 2] = np.linspace(0, 1, V)
 
-    return vs,fs,fc
+    return vs, fs, fc
+
 
 def load(filename):
     res = list()
     if not os.path.isfile(filename):
         return res
 
-    f = open(filename,'r')
+    f = open(filename, 'r')
     for l in f:
-        res += [tuple(map(eval,l.strip().split(' ')))]
+        res += [tuple(map(eval, l.strip().split(' ')))]
 
     return res
