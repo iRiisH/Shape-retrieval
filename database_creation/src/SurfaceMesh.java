8ff7cd2f577e258cfbab9b44cd31c36dc0164a86
Datoclement <70664914@qq.com>
2017-12-04 21:24:25 +0100

    comment and cut useless code in arcball and viewer

--- database_creation/src/SurfaceMesh.java
+++ database_creation/src/SurfaceMesh.java
@@ -1,52 +1,44 @@
 import Jcg.geometry.*;
 import Jcg.polyhedron.*;
 
-
-/**
- * 
- * Class for rendering a surface triangle mesh
- * @author Luca Castelli Aleardi (INF555, 2012)
- *
- */
 public class SurfaceMesh {
-	
+
 	double scaleFactor=50; // scaling factor: useful for 3d rendering
-	MeshViewer view;
+	Viewer view;
 	public Polyhedron_3<Point_3> polyhedron3D; // triangle mesh
-	
+
 	/**
 	 * Create a surface mesh from an OFF file
-	 */	
-	public SurfaceMesh(MeshViewer view, String filename) {
+	 */
+	public SurfaceMesh(Viewer view, String filename) {
 		this.view=view;
 
 		// shared vertex representation of the mesh
+		System.out.println(filename);
     	SharedVertexRepresentation sharedVertex=new SharedVertexRepresentation(filename);
     	LoadMesh<Point_3> load3D=new LoadMesh<Point_3>();
-    	
+
     	polyhedron3D=load3D.createTriangleMesh(sharedVertex.points,sharedVertex.faceDegrees,
 				sharedVertex.faces,sharedVertex.sizeHalfedges);
 
-    	//System.out.println(polyhedron3D.verticesToString());   	
-    	//System.out.println(polyhedron3D.facesToString());
     	polyhedron3D.isValid(false);
-    	    	
+
     	this.scaleFactor=this.computeScaleFactor();
 	}
-	
+
 	/**
 	 * Draw a segment between two points
-	 */	
+	 */
 	public void drawSegment(Point_3 p, Point_3 q) {
 		float s=(float)this.scaleFactor;
-		this.view.line(	(float)p.getX().doubleValue()*s, (float)p.getY().doubleValue()*s, 
-				(float)p.getZ().doubleValue()*s, (float)q.getX().doubleValue()*s, 
+		this.view.line(	(float)p.getX().doubleValue()*s, (float)p.getY().doubleValue()*s,
+				(float)p.getZ().doubleValue()*s, (float)q.getX().doubleValue()*s,
 				(float)q.getY().doubleValue()*s, (float)q.getZ().doubleValue()*s);
 	}
 
 	/**
 	 * Draw a triangle face
-	 */	
+	 */
 	public void drawTriangle(Point_3 p, Point_3 q, Point_3 r) {
 		float s=(float)this.scaleFactor;
 		view.vertex( (float)(p.getX().doubleValue()*s), (float)(p.getY().doubleValue()*s), (float)(p.getZ().doubleValue()*s));
@@ -54,37 +46,37 @@ public class SurfaceMesh {
 		view.vertex( (float)(r.getX().doubleValue()*s), (float)(r.getY().doubleValue()*s), (float)(r.getZ().doubleValue()*s));
 	}
 
-	
+
 	/**
 	 * Draw the entire mesh
 	 */
 	public void draw() {
 		this.drawAxis();
-		
+
 		view.beginShape(view.TRIANGLES);
 		for(Face<Point_3> f: this.polyhedron3D.facets) {
 			Halfedge<Point_3> e=f.getEdge();
 			Point_3 p=e.vertex.getPoint();
 			Point_3 q=e.getNext().vertex.getPoint();
 			Point_3 r=e.getNext().getNext().vertex.getPoint();
-			
+
 			view.noStroke();
 			view.fill(200,200,200,255); // color of the triangle
 			this.drawTriangle(p, q, r); // draw a triangle face
 		}
 		view.endShape();
-		
+
 		view.strokeWeight(2); // line width (for edges)
 		view.stroke(20);
 		for(Halfedge<Point_3> e: this.polyhedron3D.halfedges) {
 			Point_3 p=e.vertex.getPoint();
 			Point_3 q=e.opposite.vertex.getPoint();
-			
+
 			this.drawSegment(p, q); // draw edge (p,q)
 		}
 		view.strokeWeight(1);
 	}
-	
+
 	/**
 	 * Draw the X, Y and Z axis
 	 */
@@ -94,7 +86,7 @@ public class SurfaceMesh {
 		Point_3 p100=new Point_3(s, 0., 0.);
 		Point_3 p010=new Point_3(0.,s, 0.);
 		Point_3 p011=new Point_3(0., 0., s);
-		
+
 		drawSegment(p000, p100);
 		drawSegment(p000, p010);
 		drawSegment(p000, p011);
@@ -107,7 +99,7 @@ public class SurfaceMesh {
 	public static double round(double x, int precision) {
 		return ((int)(x*precision)/(double)precision);
 	}
-	
+
 	/**
 	 * Compute the scale factor (depending on the max distance of the point set)
 	 */
@@ -122,5 +114,5 @@ public class SurfaceMesh {
 		}
 		return Math.sqrt(3)/maxDistance*150;
 	}
-	
+
 }
---
+++ database_creation/src/Viewer.java
@@ -0,0 +1,86 @@
+import processing.core.*;
+
+/**
+ * A simple 3d viewer for visualizing surface meshes
+ *
+ * @author Luca Castelli Aleardi (INF555, 2012)
+ *
+ */
+public class Viewer extends PApplet {
+
+	SurfaceMesh mesh;
+
+	int renderType=0; // choice of type of rendering
+	int renderModes=3; // number of rendering modes
+
+	int simplificationMethod=0;
+	int nMethods=3; // number of simplification methods proposed
+	int model_id = 0;
+	static String path = "../../data/benchmark/db/";
+	String filename;
+
+	private static String get_filename(int model_id){
+		String folder = String.valueOf(model_id / 100);
+		String filename = String.valueOf(model_id);
+		return path+folder+"/m"+filename+"/m"+filename+".off";
+	}
+
+	// initialization
+	public void setup() {
+
+		// initialize window size
+	  	size(800,600,P3D);
+
+		// initialize Arcball
+	  	ArcBall arcball = new ArcBall(this);
+
+		filename = this.get_filename(model_id);
+	  	this.mesh=new SurfaceMesh(this, filename);
+	}
+
+	public void draw() {
+
+		// set the background color
+	  	background(255);
+
+		// set light
+	  	directionalLight(101, 204, 255, -1, 0, 0);
+	  	directionalLight(51, 102, 126, 0, -1, 0);
+	  	directionalLight(51, 102, 126, 0, 0, -1);
+	  	directionalLight(102, 50, 126, 1, 0, 0);
+	  	directionalLight(51, 50, 102, 0, 1, 0);
+	  	directionalLight(51, 50, 102, 0, 0, 1);
+
+		// set original position
+		/*
+			positive direction x: right (opposite to left)
+			positive direction y: down (opposite to up)
+			positive direction z: close (opposite to far)
+		*/
+	  	translate(width/2.f,height/2.f,-1*height/2.f);
+
+		// set stroke style
+	  	this.strokeWeight(1);
+	  	stroke(150,150,150);
+
+	  	this.mesh.draw();
+	}
+
+	public void keyPressed(){
+		  switch(key) {
+			case('r'):this.renderType=(this.renderType+1)%this.renderModes; break;
+		  }
+	}
+
+
+
+	/**
+	 * For running the PApplet as Java application
+	 */
+	public static void main(String args[]) {
+		//PApplet pa=new Viewer();
+		//pa.setSize(400, 400);
+		PApplet.main(new String[] { "Viewer" });
+	}
+
+}
