ab99d84f710f8f7420fd5a9905060d13f862cbaa
Datoclement <70664914@qq.com>
2017-12-06 08:58:47 +0100

    featureComputer

--- database_creation/src/FeatureComputer.java
+++ database_creation/src/FeatureComputer.java
@@ -1,76 +1,46 @@
-// import java.util.Properties;
-// import java.util.Enumeration;
-//
-// public class FeatureComputer {
-//   public static void main(String args[]) {
-//     if( args.length == 0 ) {
-//       Properties p = System.getProperties();
-//       Enumeration keys = p.keys();
-//       while (keys.hasMoreElements()) {
-//         String key = (String)keys.nextElement();
-//         String value = (String)p.get(key);
-//         System.out.println(key + " : " + value);
-//       }
-//     }
-//     else {
-//       for (String key: args) {
-//         System.out.println(System.getProperty( key ));
-//       }
-//     }
-//   }
-// }
+import org.opencv.core.*;
+import org.opencv.imgproc.*;
+import org.opencv.imgproc.Imgproc;
 
+public class FeatureComputer{
 
-// import org.opencv.core.Core;
-// import org.opencv.core.CvType;
-// import org.opencv.core.Mat;
-//
-// public class FeatureComputer
-// {
-//    public static void main( String[] args )
-//    {
-//        // System.out.println(Core.NATIVE_LIBRARY_NAME);
-//       // System.loadLibrary( "opencv-331" );
-//       Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
-//       System.out.println( "mat = " + mat.dump() );
-//    }
-// }
+    private class GaborFilter{
 
-// //
-// // public class FeatureComputer{
-// //
-// //     private class GaborFilter{
-// //
-// //         float prf;
-// //         float fo;
-// //         float fb;
-// //         float ab;
-// //
-// //         public GaborFilter(float prf,float fo,float fb,float ab){
-// //             this.prf = prf;
-// //             this.fo = fo;
-// //             this.fb = fb;
-// //             this.ab = ab;
-// //         }
-// //
-// //         public Mat apply(Mat m){
-// //             Mat padded;
-// //             int r = image.rows(), c = image.cols();
-// //             int opt_r = Core.getOptimalDFTSize(r);
-// //             int opt_c = Core.getOptimalDFTSize(c);
-// //             Core.copyMakeBorder(
-// //                     image,
-// //                     padded,
-// //                     0,
-// //                     opt_r-r,
-// //                     0,
-// //                     opt_c-c,
-// //                     Imgproc.BORDER_CONSTANT,
-// //                     Scalar.all(0));
-// //         }
-// //     }
-// //
-// //     public FeatureComputer(float prfs,float fos,float fbs,float abs){
-// //
-// //     }
-// // }
+        float prf;
+        float fo;
+        float fb;
+        float ab;
+
+        public GaborFilter(float prf,float fo,float fb,float ab){
+            this.prf = prf;
+            this.fo = fo;
+            this.fb = fb;
+            this.ab = ab;
+        }
+
+        public Mat apply(Mat image){
+            Mat padded = null;
+            int r = image.rows(), c = image.cols();
+            int opt_r = Core.getOptimalDFTSize(r);
+            int opt_c = Core.getOptimalDFTSize(c);
+            Core.copyMakeBorder(
+                    image,
+                    padded,
+                    0,
+                    opt_r-r,
+                    0,
+                    opt_c-c,
+                    Core.BORDER_DEFAULT,
+                    Scalar.all(0));
+            throw new Error("Non implemented");
+        }
+    }
+
+    public FeatureComputer(float prfs,float fos,float fbs,float abs){
+
+    }
+
+    public static void main(String[] args){
+        return;
+    }
+}
